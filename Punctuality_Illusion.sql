6b9fa60f384c11daf30424ba9982568f62058707
AylinYildiz26 <aylinyildiz1@gmx.net>
2026-03-01 20:51:12 +0100

    Create Punctuality_Illusion.sql
    first add of query Punctuality_Illusion.sql

---
+++ Punctuality_Illusion.sql
@@ -0,0 +1,17 @@
+SELECT 
+    CASE 
+        WHEN DATEDIFF(order_delivered_customer_date, order_estimated_delivery_date) > 0 
+        THEN 'Delayed' 
+        ELSE 'On time'
+    END AS delivery_status,                                     -- Categorizing by system logic
+    COUNT(DISTINCT order_id) AS orders_count                    -- Unique order count for tech
+FROM orders o
+JOIN order_items oi USING (order_id)                            -- Linking orders to items
+JOIN products p USING (product_id)                              -- Accessing product categories
+WHERE order_status = 'delivered'                                -- Excluding canceled/pending
+    AND order_estimated_delivery_date IS NOT NULL               -- Ensuring date integrity
+    AND order_delivered_customer_date IS NOT NULL               -- Ensuring delivery occurred
+    AND p.product_category_name IN (                            -- Filtering for Eniac's core tech
+        'informatica_acessorios', 'pcs', 'telefonia',
+        'eletronicos', 'tablets_impressao_imagem')
+GROUP BY delivery_status;                                       -- Pivot for "On Time" ratio
